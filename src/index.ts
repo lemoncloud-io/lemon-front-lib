8d5e5f17e7d4c6eed8927362f4d74c5ef5fc1507
louis <louis@lemoncloud.io>
2019-12-06 14:07:33 +0900

    chore: export other services

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+// services
+export * from './services/aws.service';
+export * from './services/cognito-http.service';
+export * from './services/cognito.service';
+export * from './services/http.service';
+export * from './services/sig-v4.service';
 export * from './services/core.service';
 
+// types
 export * from './types/cognito.interface';
 export * from './types/authentication-state.type';
 export * from './types/forgot-password-state.type';

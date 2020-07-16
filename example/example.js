a23298e1b11e1538c33a9a2594100c7f9c6b9ae6
louis <louis@lemoncloud.io>
2020-07-16 18:15:46 +0900

    chore: add example case

---
+++ example/example.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+
+const app = http.createServer(function(request,response){
+    let url = request.url;
+    const shouldReturnIndex = request.url === '/' || request.url.includes('code') || request.url.includes('favicon.ico');
+    if (shouldReturnIndex) {
+        url = '/index.html';
+    }
+    if (request.url.includes('lemon.front.bundle.js')) {
+        url = `/..${request.url}`;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+});
+
+app.listen(8888);
+console.log('start localhost:8888');
